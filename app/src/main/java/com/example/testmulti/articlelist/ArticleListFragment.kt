498b28ad8b7d8521cd043cd7c53e35c1abc3d4a2
Sokhibdzhon <sokhibsaid@gmail.com>
2021-09-22 10:53:58 +0400

    Getting List of Apps according to action, Clipboard Usage

--- app/src/main/java/com/example/testmulti/articlelist/ArticleListFragment.kt
+++ app/src/main/java/com/example/testmulti/articlelist/ArticleListFragment.kt
@@ -1,7 +1,9 @@
 package com.example.testmulti.articlelist
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.viewModels
 import com.example.common.base.BaseFragment
 import com.example.common.extension.observe
@@ -9,16 +11,52 @@ import com.example.testmulti.R
 import com.example.testmulti.databinding.ArticleListFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class ArticleListFragment :
     BaseFragment<ArticleListViewModel, ArticleListFragmentBinding>(R.layout.article_list_fragment) {
 
     private val articleVM: ArticleListViewModel by viewModels()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding!!.viewmodel = articleVM
         observe(articleVM.state, ::setCircleState)
+
+        binding!!.circleView.setOnClickListener {
+            //List of apps handling SEND Action
+//            val pm = requireActivity().packageManager
+//            val mainIntent = Intent(Intent.ACTION_SEND, null)
+//            mainIntent.putExtra("Zibab", "Ivj")
+//            mainIntent.type = "text/plain"
+//            val resolveInfos = pm.queryIntentActivities(
+//                mainIntent,
+//                0
+//            )
+//            for (info in resolveInfos) {
+//                val applicationInfo = info.activityInfo.applicationInfo
+//                Timber.d("${applicationInfo.loadLabel(pm)}")
+//                Timber.d(applicationInfo.packageName)
+//            }
+
+
+            //Night Mode Check
+            val uiModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            //Change mode with Act-recreation
+            if (uiModeFlags == Configuration.UI_MODE_NIGHT_NO) {
+                AppCompatDelegate
+                    .setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else AppCompatDelegate
+                .setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+            //Clipboard set
+//            val clipboard: ClipboardManager? =
+//                requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+//            val clip = ClipData.newPlainText("label", "text")
+//            clipboard?.setPrimaryClip(clip)
+
+        }
+
     }
 
 
