68b669bfef50d852f6d0bf555304c8c2d027a806
Sokhibdzhon <sokhibsaid@gmail.com>
2021-10-03 10:54:51 +0400

    Mapper test added

--- app/src/test/java/com/example/testmulti/model/ArticleListMapperTest.kt
+++ app/src/test/java/com/example/testmulti/model/ArticleListMapperTest.kt
@@ -3,7 +3,9 @@ package com.example.testmulti.model
 import com.example.data.mapper.ArticleListMapper
 import com.example.data.model.PopularArticles
 import com.example.data.model.Result
+import com.example.domain.model.ArticleModel
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Test
 
 class CharacterDetailMapperTest {
@@ -18,6 +20,15 @@ class CharacterDetailMapperTest {
                 results = listOf(Result(null, null, null, null, null))
             )
             mapper.map(response)
+        }
+    }
+
+    @Test
+    fun nullList_Articles_ShouldReturnEmptyList() {
+        runBlocking {
+            val response = PopularArticles(null, "NO_DATA")
+            val result = mapper.map(response)
+            Assert.assertEquals(result, emptyList<ArticleModel>())
         }
     }
 
