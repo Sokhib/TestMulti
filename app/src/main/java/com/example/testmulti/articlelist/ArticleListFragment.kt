29a6ea46185cb1e9d70910d325792a47ef62ad40
Sokhibdzhon <sokhibsaid@gmail.com>
2021-10-13 11:35:16 +0400

    Runtime Permission after API level 23 code sample

--- app/src/main/java/com/example/testmulti/articlelist/ArticleListFragment.kt
+++ app/src/main/java/com/example/testmulti/articlelist/ArticleListFragment.kt
@@ -1,93 +1,72 @@
 package com.example.testmulti.articlelist
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
-import android.view.animation.OvershootInterpolator
-import androidx.core.app.ActivityCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.example.common.base.BaseFragment
 import com.example.common.extension.observe
 import com.example.testmulti.R
-import com.example.testmulti.RecordController
-import com.example.testmulti.TestIntentActivity
 import com.example.testmulti.databinding.ArticleListFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.FlowPreview
 import timber.log.Timber
 
 
+@FlowPreview
 @AndroidEntryPoint
 class ArticleListFragment :
     BaseFragment<ArticleListViewModel, ArticleListFragmentBinding>(R.layout.article_list_fragment) {
 
     private val articleVM: ArticleListViewModel by viewModels()
-    private val recordController by lazy { RecordController(requireActivity()) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding!!.viewmodel = articleVM
         observe(articleVM.state, ::setCircleState)
-
-        binding!!.circleView.setOnClickListener {
-            startActivity(Intent(requireActivity(), TestIntentActivity::class.java))
-        }
-        requestPermission()
-        binding!!.sliderView.setOnClickListener {
-            onButtonClicked()
-        }
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            requireActivity(),
-            arrayOf(android.Manifest.permission.RECORD_AUDIO),
-            101,
-        )
-    }
-
-    private fun onButtonClicked() {
-        lifecycleScope.launch(Dispatchers.Default) {
-            if (recordController.isAudioRecording()) {
-                recordController.stop()
-
-            } else {
-                recordController.start()
-                repeat(100_000) {
-                    delay(100)
-                    val volume = recordController.getVolume()
-                    withContext(Dispatchers.Main) {
-                        handleVolume(volume)
-                    }
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Timber.d("Permission is: $isGranted")
+                } else {
+                    Timber.d("Bhaijan look we need this permission")
                 }
             }
 
+
+
+        binding!!.circleView.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    Timber.d("Bhaijan permission is already granted. What are you doing...")
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                    Timber.d("Bhaijan this is education UI for the permission")
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+                else -> {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+
+                }
+            }
         }
-
     }
 
-    private fun handleVolume(volume: Int) {
-        Timber.d("Volume: $volume")
-        val scale = 10.0.coerceAtMost(volume / MAX_RECORD_AMPLITUDE + 1.0f).toFloat()
-        binding!!.circleView.animate()
-            .scaleX(scale)
-            .scaleY(scale)
-            .setInterpolator(interpolator)
-            .duration = VOLUME_UPDATE_DURATION
-    }
 
     override fun getViewModel() = articleVM
     private fun setCircleState(state: Boolean) {
         if (state) binding!!.circleView.setChecked()
-    }
-
-    private companion object {
-        private const val MAX_RECORD_AMPLITUDE = 32768.0
-        private const val VOLUME_UPDATE_DURATION = 100L
-        private val interpolator = OvershootInterpolator()
     }
 }
 
